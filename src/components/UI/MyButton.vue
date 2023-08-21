f6bbf6b3a8328713b2048c71dd87fde3614d1738
Stanislav <stanislav.nechaev@gmail.com>
2023-08-22 00:02:18 +0300

    add dynamic button

--- src/components/UI/MyButton.vue
+++ src/components/UI/MyButton.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <button class="m-2.5 py-2.5 px-3.5 text-blue-500 border-solid border-2 rounded border-blue-500">
     <slot></slot>
   </button>
@@ -8,6 +8,20 @@
 export default {
 name: "my-button"
 }
+</script> -->
+
+<template>
+  <button
+    class="m-2.5 py-2.5 px-3.5 text-blue-500 border-solid border-2 rounded border-blue-500 transition-bg duration-300 hover:bg-blue-500 hover:text-white"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "my-button",
+};
 </script>
 
 <style scoped>
