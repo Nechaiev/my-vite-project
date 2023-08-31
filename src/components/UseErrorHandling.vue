4c11ee4f16953d81d89ff39b05c6e9a40c9a7395
Stanislav <stanislav.nechaev@gmail.com>
2023-08-31 03:52:07 +0300

    add toast and loading

---
+++ src/components/UseErrorHandling.vue
@@ -0,0 +1,22 @@
+<script>
+import { ref } from "vue";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+export default function useErrorHandling() {
+  const error = ref(null);
+
+  const showError = (message) => {
+    error.value = message;
+    Toast.open({
+      message,
+      type: "error",
+    });
+  };
+
+  return {
+    error,
+    showError,
+  };
+}
+</script>
---
+++ src/components/UseLoading.vue
@@ -0,0 +1,21 @@
+<script>
+import { ref } from "vue";
+
+export default function useLoading() {
+  const isLoading = ref(false);
+
+  const startLoading = () => {
+    isLoading.value = true;
+  };
+
+  const stopLoading = () => {
+    isLoading.value = false;
+  };
+
+  return {
+    isLoading,
+    startLoading,
+    stopLoading,
+  };
+}
+</script>
